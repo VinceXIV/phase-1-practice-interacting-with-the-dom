75aae2f798c0d82b4947fd621278db0ccb0a3738
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-22 13:06:54 +0300

    Process stuff to do with the pause button

--- js/challenge.js
+++ js/challenge.js
@@ -22,16 +22,13 @@ function resumeCounter(){
 
 
 // process stuff related to the minus and the plus
-
-document.getElementById('minus')
-.addEventListener('click', event => {
+function subtractOne (){
     counter.textContent = parseInt(counter.textContent) - 1;
-})
+}
 
-document.getElementById('plus')
-.addEventListener('click', event => {
+function addOne () {
     counter.textContent = parseInt(counter.textContent) + 1;
-})
+}
 
 
 // process stuff related to the heart button
@@ -48,8 +45,7 @@ function getClickCount(number){
     return `${number} has been liked ${numberAndClickCount[number]} times`
 }
 
-document.getElementById('heart')
-.addEventListener('click', event => {
+function updateClickCount() {
     const likedNumbers = document.querySelector('ul.likes')
 
     let currLikedNumber = document.getElementById(`number-${counter.textContent}`)
@@ -62,6 +58,79 @@ document.getElementById('heart')
 
         likedNumbers.appendChild(currLikedNumber)
     }
+}
+
+
+// Start listening and stop listening
+
+function startListening(){
+    
+    document.getElementById('minus').addEventListener('click', subtractOne)
+    document.getElementById('plus').addEventListener('click', addOne)
+    document.getElementById('heart').addEventListener('click', updateClickCount)
+}
+
+function stopListening(){
+    
+    document.getElementById('minus').removeEventListener('click', subtractOne)
+    document.getElementById('plus').removeEventListener('click', addOne)
+    document.getElementById('heart').removeEventListener('click', updateClickCount)
+}
+
+
+startListening();
+
+
+// process stuff related to the pause button
+
+function disableButtons(){
+    const btns = Array.from(document.querySelectorAll('button'))
+    for(const btn of btns){
+        btn.classList.add('inactive')
+        btn.classList.remove('active')
+    }
+}
+
+function enableButtons(){
+    const btns = Array.from(document.querySelectorAll('button'))
+    for(const btn of btns){
+        btn.classList.add('active')
+        btn.classList.remove('inactive')
+    }
+}
+
+
+function changeButtonStyle(){
+    const inactiveBtns = Array.from(document.getElementsByClassName('inactive'))
+    const activeBtns = Array.from(document.getElementsByClassName('active'))
+
+    for(const btn of inactiveBtns){
+        if(btn.id == 'pause'){
+            continue;
+        }
+        btn.style.opacity = '0.5'
+    }
+
+    for(const btn of activeBtns){
+        btn.style.opacity = '1'
+    }
+}
+
+
+const pauseButton = document.getElementById('pause')
+
+pauseButton.addEventListener('click', event =>{    
+    if(pauseButton.textContent == ' pause '){
+        pauseCounter();
+        pauseButton.textContent = ' resume '
+        stopListening();
+        disableButtons();
+        changeButtonStyle()
+    }else {
+        resumeCounter();
+        pauseButton.textContent = ' pause '
+        startListening()
+        enableButtons()
+        changeButtonStyle()
+    }
 })
-
-
