d99fef0d9815faf18197c32d4afc5e3f6e6ec90c
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-22 12:10:54 +0300

    Process stuff related to the heart button

--- js/challenge.js
+++ js/challenge.js
@@ -21,3 +21,47 @@ function resumeCounter(){
 }
 
 
+// process stuff related to the minus and the plus
+
+document.getElementById('minus')
+.addEventListener('click', event => {
+    counter.textContent = parseInt(counter.textContent) - 1;
+})
+
+document.getElementById('plus')
+.addEventListener('click', event => {
+    counter.textContent = parseInt(counter.textContent) + 1;
+})
+
+
+// process stuff related to the heart button
+
+const numberAndClickCount = {}
+
+function getClickCount(number){
+    if(Object.keys(numberAndClickCount).includes(number)){
+        numberAndClickCount[number] += 1;
+    }else{
+        numberAndClickCount[number] = 1;
+    }
+
+    return `${number} has been liked ${numberAndClickCount[number]} times`
+}
+
+document.getElementById('heart')
+.addEventListener('click', event => {
+    const likedNumbers = document.querySelector('ul.likes')
+
+    let currLikedNumber = document.getElementById(`number-${counter.textContent}`)
+    if(currLikedNumber){
+        currLikedNumber.textContent = getClickCount(counter.textContent)
+    }else {
+        currLikedNumber = document.createElement('li')
+        currLikedNumber.id = `number-${counter.textContent}`
+        currLikedNumber.textContent = getClickCount(counter.textContent)
+
+        likedNumbers.appendChild(currLikedNumber)
+    }
+})
+
+
